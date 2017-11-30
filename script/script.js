8e58a3a64c9d8a0e24a7b22ac43765b8148516d2
MoerkerkeLander <lander.moerkerke@howest.student.be>
2017-11-30 21:08:32 +0100

    Finished full cycle

--- script/script.js
+++ script/script.js
@@ -1,5 +1,5 @@
 // API SETTINGS
-var _AMOUNT_QUESTIONS = 1;
+var _AMOUNT_QUESTIONS = 5;
 
 var _GETALLCATEGORIES_LINK = 'https://opentdb.com/api_category.php';
 var _GETQUESTION_LINK = 'https://opentdb.com/api.php?amount=' + String(_AMOUNT_QUESTIONS);
@@ -10,11 +10,16 @@ var cboCategory,
 	cboType,
 	btnShoot,
 	btnPrefs,
-	headerSlogan,
-	sectPrefs,
-	sectQuestions,
-	homepageContainer,
-	answersContainer;
+	main__content,
+	header__slogan,
+	main__content__preferences,
+	main__content__question,
+	main__content__container,
+	main__content__answers,
+	main__content__response;
+
+// QUESTIONS
+var arrQuestions;
 
 // SETUP
 function GetElements() {
@@ -23,11 +28,27 @@ function GetElements() {
 	cboType = $('#cboType');
 	btnShoot = $('#btnShoot');
 	btnPrefs = $('#btnPrefs');
-	headerSlogan = $('.header__slogan');
-	sectPrefs = $('.main__content__preferences');
-	sectQuestions = $('.main__content__question');
-	homepageContainer = $('.main__content__conainer');
-	answersContainer = $('.main__content__answers');
+
+	// main, root
+	main__content = $('.main__content');
+
+	// h1 in the header
+	header__slogan = $('.header__slogan');
+
+	// container buttons, select
+	main__content__container = $('.main__content__container');
+
+	// container of the selects
+	main__content__preferences = $('.main__content__preferences');
+
+	// containter of the question (card)
+	main__content__question = $('.main__content__question');
+
+	// container of the buttons
+	main__content__answers = $('.main__content__answers');
+
+	// container of response
+	main__content__response = $('.main__content__response');
 }
 
 // CATEGORIES
@@ -58,6 +79,7 @@ function SortCategories(unsortedCategories) {
 }
 
 function FillCategoriesComboBox(categories) {
+	// cboCategory.empty();
 	var itemval;
 	for (var i = categories.length - 1; i >= 0; i--) {
 		if (i !== undefined) {
@@ -72,10 +94,10 @@ function FillCategoriesComboBox(categories) {
 var boolShowPrefs;
 function ShowPreferences() {
 	if (boolShowPrefs) {
-		sectPrefs.css('display', 'none');
+		main__content__preferences.css('display', 'none');
 	}
 	else {
-		sectPrefs.css('display', 'flex');
+		main__content__preferences.css('display', 'flex');
 	}
 	boolShowPrefs = !boolShowPrefs
 }
@@ -108,16 +130,9 @@ function GetQuestion(requestLink) {
 	$.getJSON(requestLink, function (data) {
 		if (data['response_code'] == 0) {
 			// SUCCES
-
-			// homepageContainer.css('display', 'none');
-			changeDisplayProperty(homepageContainer, 'none');
-			// $('.header__slogan').css('display', 'none');
-			changeDisplayProperty(headerSlogan, 'none');
-			// $('.main__content__answers').css('display', 'grid');
-			changeDisplayProperty($('.main__content__answers'), 'grid');
-			// $('.main__content__question').css('display', 'block');
-			changeDisplayProperty($('.main__content__question'), 'block');
-			
+			hideOtherContainersButQuestion();
+			arrQuestions = data['results'];
+			console.log(arrQuestions);
 			showQuestion(data['results']);			
 		}
 		else {
@@ -128,24 +143,32 @@ function GetQuestion(requestLink) {
 	})
 }
 
+function hideOtherContainersButQuestion() {
+	changeDisplayProperty(main__content__container, 'none');
+	changeDisplayProperty(header__slogan, 'none');
+	changeDisplayProperty($('.main__content__answers'), 'grid');
+	changeDisplayProperty($('.main__content__question'), 'block');
+}
+
 function showQuestion(questions) {
 	var question = questions[0];
-	// SHOW CATEGORY
-	sectQuestions.append('<div class="question__card"><p class="question__card__category">' + question['category']  + '</p><p class="question__card__question">' + question['question'] + '</p></div>');
+	main__content__question.empty();
+	main__content__question.append('<div class="question__card"><p class="question__card__category">' + question['category']  + '</p><p class="question__card__question">' + question['question'] + '</p></div>');
 
-	
+	main__content__answers.empty();
+
 	if (question['type'] == "multiple") {
 		var answers = question['incorrect_answers'];
 		answers.push(question['correct_answer']);
 		answers.sort();
 
 		for (var index = 0; index < answers.length; index++) {
-			createButtonAnswer(answers[index], answersContainer);
+			createButtonAnswer(answers[index], main__content__answers);
 		}
 	}
 	else {
-		createButtonAnswer('True', answersContainer);
-		createButtonAnswer('False', answersContainer);	
+		createButtonAnswer('True', main__content__answers);
+		createButtonAnswer('False', main__content__answers);	
 	}
 
 	$('.btnAnswer').on('click', function(e) {
@@ -158,36 +181,64 @@ function createButtonAnswer(pText, pContainer){
 }
 
 function checkQuestion(pSelected, pCorrect){
-	answersContainer.css('display', 'none');
-	sectQuestions.css('display', 'none');
+	var text;
+	hideQuestion();
+	main__content__response.empty();
 	if (pSelected == pCorrect) {
-		// hideQuestion();
-		console.log("juist");
 		showCorrect();
 	}
 	else {
-		console.log("fout");
-		// hideQuestion();
-		showIncorrect();
+		showCorrect();
 	}
+
+	$('.btnNewQuestion').click(newQuestion);
 }
 
 function hideQuestion(){
-	answersContainer.css('display', 'none');
-	sectQuestions.css('display', 'none');
+	main__content__answers.css('display', 'none');
+	main__content__question.css('display', 'none');
 }
 
+function newQuestion() {
+	// main__content__answers.remove();
+	// main__content__question.remove();
+	// main__content__preferences
+	// main__content__question
+	// main__content__container
+	// main__content__answers
+	// main__content__response
+
+	main__content.removeClass('correct incorrect');
+	changeDisplayProperty(main__content__response, 'none');
+	Shoot();
+
+}
 // END QUESTIONS
 
 
 // SHOW SUCCESS
 function showCorrect(){
-
+	main__content__response.css('display', 'grid');
+	main__content__response.append(
+		$('<?xml version="1.0" encoding="UTF-8"?><svg viewBox="0 0 148.11 119.31" xmlns="http://www.w3.org/2000/svg"><defs><style>.svgcolor{fill:#cecece;}</style></defs><g data-name="Layer 2"><g data-name="Eindwerk"><g data-name="QuestionSucces"><polygon class="svgcolor" points="125.98 0 50.94 75.05 22.12 46.24 0 68.36 28.81 97.17 28.8 97.18 50.92 119.31 148.11 22.12"/></g></g></g></svg>'),
+		$('<p/>', { text: 'Great, succes!' }),
+		$('<button/>', { class: 'btnNewQuestion', text: 'New question!' })
+	);
+			
+	main__content.addClass('correct');	
 }
 
 // SHOW FAIL
 function showIncorrect() {
-	
+	main__content__response.css('display', 'table');
+
+	main__content__response.append(
+		$('<?xml version="1.0" encoding="UTF-8"?><svg viewBox="0 0 119.7 120.7" xmlns="http://www.w3.org/2000/svg"><defs><style>.svgcolorIcc{fill:#aa1818;}</style></defs><title>Asset 10</title><g data-name="Layer 2"><g data-name="Eindwerk"><g data-name="QuestionSucces2"><polygon class="svgcolorIcc" points="119.7 23.13 97.57 1 60.35 38.22 22.13 0 0 22.13 38.22 60.35 0 98.57 22.13 120.7 60.35 82.48 97.57 119.7 119.7 97.57 82.48 60.35"/></g></g></g></svg>'),
+		$('<p/>', { text: 'Let‘s pretend that never happened!' }),
+		$('<button/>', { class: 'btnNewQuestion', text: 'Try again!' })
+	);
+
+	main__content.addClass('incorrect');
 }
 
 // VARIA
