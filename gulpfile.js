ea3193896a14c59007175a88deb33c28f023f65b
MoerkerkeLander <lander.moerkerke@howest.student.be>
2017-12-03 15:09:51 +0100

    Implemented Google Analytics

--- gulpfile.js
+++ gulpfile.js
@@ -7,12 +7,28 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify'),
 	rename = require('gulp-rename'),
 	jquery = require('jquery'),
-	ggf = require('gulp-google-fonts');
+	ggf = require('gulp-google-fonts'),
+	ga = require('gulp-ga');
 
 gulp.task('font', function () {
 	return gulp.src('config_fonts.neon')
 		.pipe(ggf())
 		.pipe(gulp.dest('fonts'));
+});
+
+gulp.task('ga', function () {
+	gulp.src('./dist/index.html')
+		.pipe(
+			ga({ 
+				url: 'moerkerkelander.github.io/dist/', 
+				uid: 'UA-110468690-1',
+				anonymizeIp: false,
+				demographics: true,
+				linkAttribution: true,
+				minify: true
+			})
+		)
+		.pipe(gulp.dest('./dist/'));
 });
 
 gulp.task('css', function(){
